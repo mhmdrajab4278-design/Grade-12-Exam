02279ab615701ce05f0758f590d2ebfd7ef9933a
mhmdrajab4278-design <mhmdrajab4278@gmail.com>
2026-09-26 01:54:05 +0300

    Update categories and quiz navigation functionality
    Added a new category 'Capitals' with 196 questions and updated event handling for quiz navigation.

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ let category = [
         "num": "NO. of Questions: 50",
         "subject": "Subject English"
     },
+    {
+        "name": "Capitals",
+        "num": "NO. of Questions: 196",
+        "subject": "General Geography"
+    }
 ]
 const cards = document.getElementById("cards")
 
@@ -36,8 +41,29 @@ async function start() {
     const sidebtn = document.getElementById("sidebtn");
     const hideside = document.getElementById("hideside");
 
+    const squiz = document.getElementById("squiz");
+    const quiz = document.getElementById("quiz");
+    const lquiz = document.getElementById("lquiz");
+    const fexam = document.getElementById("fexam");
+    const submit = document.getElementById("submit");
+    const cancel = document.getElementById("cancel");
+    const next = document.getElementById("next");
+    const foot = document.querySelector(".foot");
+    const quiz_form = document.getElementById("quiz-form");
+    let selected;
+    let elementoutside;
+    
+    cancel.addEventListener("click", event => {
+        foot.classList.remove("active-foot");
+    })
+    
+    quiz_form.addEventListener("change", event => {
+        selected = event.target;
+    })
+
+
     sidebtn.addEventListener("click", event => {
-        side.classList.add("active")
+        side.classList.add("active");
     })
 
     hideside.addEventListener("click", event => {
@@ -65,14 +91,17 @@ async function start() {
         cards.appendChild(card)
         
         card.addEventListener("click", event => {
-            window.location.href = `index2.html?test=${element.name}`;
+            // window.location.href = `index2.html?test=${element.name}`;
+            elementoutside = element;
+            foot.classList.add("active-foot")
         })
 
+        
         const allcards = document.querySelectorAll(".card")
-
+        
         select.addEventListener("change", event => {
             const color = event.target.value;
-    
+            
             if(color == "dodgerblue"){
                 document.documentElement.style.setProperty("--primary-color", color)
             }
@@ -80,32 +109,35 @@ async function start() {
             else if(color == "lightgreen"){
                 document.documentElement.style.setProperty("--primary-color", color)
             }
-    
+            
             else if(color == "tomato"){
                 document.documentElement.style.setProperty("--primary-color", color)
             }
-    
+            
             else if(color == "orange"){
                 document.documentElement.style.setProperty("--primary-color", color)
             }
-    
+            
             window.localStorage.setItem("theme", color);
         })
-    
+        
         const savedcolor = localStorage.getItem("theme");
-    
+        
         if(savedcolor){
             document.documentElement.style.setProperty("--primary-color", savedcolor)
         }
+    })
+    next.addEventListener("click", event => {
+        window.location.href = `index2.html?test=${elementoutside.name}&type=${selected.id}`;
     })
 }
 
 start()
 
 /*
-    <div class="card">
-        <h1 id="qname">25-25 T1</h1>
-        <p id="numq">NO. of Questions: 50</p>
-        <p id="subject">English Test</p>
-    </div>
+<div class="card">
+<h1 id="qname">25-25 T1</h1>
+<p id="numq">NO. of Questions: 50</p>
+<p id="subject">English Test</p>
+</div>
 */
